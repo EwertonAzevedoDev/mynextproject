35bab0ca6da4b5cdd22f78564e74d0ff4a8ce7d0
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-22 15:21:17 -0300

    Pagina Sobre

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home(){
-    return <div><h1>Home</h1></div>
+    return (<div>
+                <h1>Home</h1>
+                <a href="/sobre">Acessar página Sobre</a>    
+            </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,8 @@
+function Sobre(){
+    return (<div>
+                <h1>Sobre</h1>
+                <a href="/">Acessar página Sobre</a>    
+            </div>)
+}
+
+export default Sobre
