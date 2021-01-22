3861ff784039dbb032d652e673922d406e178ef9
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-22 15:30:32 -0300

    Component Link fix

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Sobre(){
     return (<div>
                 <h1>Sobre</h1>
-                <Link href="/"></Link>
+                <Link href="/">
                     <a>Acessar página Home</a>
                 </Link>
             </div>)
