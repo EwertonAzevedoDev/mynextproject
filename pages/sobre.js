ec25231cebf155f449a3a0e030ffbe6f8a66830a
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-22 15:26:04 -0300

    Component Link

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 function Sobre(){
     return (<div>
                 <h1>Sobre</h1>
-                <a href="/">Acessar página Sobre</a>    
+                <Link href="/"></Link>
+                    <a>Acessar página Home</a>
+                </Link>
             </div>)
 }
 
