ff0ef64e883e622000dbc6564f1c7fbea29c2f15
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-22 22:17:05 -0300

    add all champions

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,12 @@
-function tempo(request, response) {
+async function tempo(request, response) {
     const dynamicDate = new Date();
 
+    const allChampionsResponse = await fetch("http://ddragon.leagueoflegends.com/cdn/11.2.1/data/pt_BR/champion.json");
+    const allChampionsResponseJson = await allChampionsResponse.json();
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        champions: allChampionsResponseJson
     });
 }
 
