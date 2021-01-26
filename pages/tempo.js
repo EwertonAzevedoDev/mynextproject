73365434fb039fe3edc301f08fd9e7a01aff67d3
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-26 10:34:48 -0300

    Console log

--- pages/tempo.js
+++ pages/tempo.js
@@ -4,8 +4,8 @@ function Tempo(props){
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
     const dados = props.championsData  
-    console.log(dados)
-    const champs = Object.values(dados).map((dado) =>
+    console.log(dados.data)
+    const champs = Object.values(dados.data).map((dado) =>
        
         <div key={dado.key}>
              <img src={"http://ddragon.leagueoflegends.com/cdn/11.2.1/img/champion/" + dado.id + ".png"}/>  
