b02f6475743e3d6c63926667dfdae68145deeb7b
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-26 10:26:43 -0300

    Console log

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -3,6 +3,7 @@ async function tempo(request, response) {
 
     const allChampionsResponse = await fetch("http://ddragon.leagueoflegends.com/cdn/11.2.1/data/pt_BR/champion.json");
     const allChampionsResponseJson = await allChampionsResponse.json();
+    console.log(allChampionsResponseJson)
 
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
