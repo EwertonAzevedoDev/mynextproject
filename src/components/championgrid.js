1f0284f61c682ae8f4a6dafc9559410ca6e3ccd8
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-29 00:31:20 -0300

    DONE search engine

--- src/components/championgrid.js
+++ src/components/championgrid.js
@@ -2,18 +2,17 @@ import styles from '../../styles.module.css'
 import { useState } from "react";
 
 function ChampionGrid({champions, needle}){ 
-    console.log(needle) 
-    const [dados, setDados] = useState(champions.allChampionsResponseJson.data)
     
-    //Fazer funcao que filtra os dados pelo que vier no needle
-    
-    
+    const [dados, setDados] = useState(champions)
   
     const champs = Object.values(dados).map((dado, index) =>       
-        <div className={styles.championsGrid} key={index}>
+        <div className={styles.championsGrid} key={dado.id}>
              <img className={styles.imageskew} src={"http://ddragon.leagueoflegends.com/cdn/11.2.1/img/champion/" + dado.id + ".png"}/>  
         </div>
-    )    
+    ).filter(function(el) {
+        return el.key.toLowerCase().indexOf(needle.toLowerCase()) > -1;
+    })   
+    
     return (                
         <div>{champs}</div>             
     )
--- src/components/championsearch.js
+++ src/components/championsearch.js
@@ -4,14 +4,15 @@ import { useState } from "react";
 
 function ChampionSearch({champions}){    
     const [needle, setNeedle] = useState('')
-
+    const [championList, setChampionList] = useState(champions.allChampionsResponseJson.data)
     const search = (event) => {
-        setNeedle(event.target.value)
+        setNeedle(event.target.value)       
     }
+   
     return (    
         <div className={styles.championPool}>
              <div className={styles.searchBar}><label>Procurar: </label><input type="text" onChange={search} value={needle} /></div> 
-            <ChampionGrid champions={champions} needle={needle}/>    
+            <ChampionGrid champions={championList} needle={needle}/>    
         </div> 
     )
 }
