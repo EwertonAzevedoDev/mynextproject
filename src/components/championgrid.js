6a24adc8d46814f414abb0ff2a599696b4ba8ef7
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-01-31 12:17:41 -0300

    Testing

--- src/components/championgrid.js
+++ src/components/championgrid.js
@@ -4,17 +4,21 @@ import { useState } from "react";
 function ChampionGrid({champions, needle}){ 
     
     const [dados, setDados] = useState(champions)
-  
+    const showlog = () => {
+        
+        console.log(champs.props.champion)
+    }
     const champs = Object.values(dados).map((dado, index) =>       
-        <div className={styles.championsGrid} key={dado.id}>
+        <div className={styles.championsGrid} key={dado.id} champion={dado}>
              <img className={styles.imageskew} src={"http://ddragon.leagueoflegends.com/cdn/11.2.1/img/champion/" + dado.id + ".png"}/>  
         </div>
     ).filter(function(el) {
+        console.log(el)
         return el.key.toLowerCase().indexOf(needle.toLowerCase()) > -1;
     })   
     
     return (                
-        <div>{champs}</div>             
+        <div onClick={showlog}>{champs}</div>             
     )
 }
 
